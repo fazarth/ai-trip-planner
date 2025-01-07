738d9a92bb1e87c36839ccda61e7931d079755d9
Tezza Fazar Tri Hidayat <37235447+fazarth@users.noreply.github.com>
2025-01-07 02:11:21 +0000

    first commit +add home page

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hero from './components/custom/Hero'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      { /* HERO */ }
+      <Hero/>
+    </>
+  )
+}
+
+export default App
