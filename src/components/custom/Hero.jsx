93c71e714c589360a5466d045711b96c12dbd427
Tezza Fazar Tri Hidayat <37235447+fazarth@users.noreply.github.com>
2025-01-09 03:27:54 +0000

    add google maps API for location

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Button } from '../ui/button'
+import { Link } from 'react-router'
 
 function Hero() {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
         <h1 className='font-extrabold text-[50px] text-center mt-16'><span className='text-[#f56551]'>Discover Your Next Adventure with AI: </span> Pernolized Itinenaries at Your Fingertips</h1>
         <p className='text-xl text-gray-500 text-center'>Your personal trip planner and trace curator, creating custime intineraries tailored to your interests and budget</p>
-        <Button>Get Started, It's Free</Button>        
+        
+        <Link to={'/create-trip'}>
+            <Button>Get Started, It's Free</Button>        
+        </Link>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Memuat file .env sesuai mode
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+    define: {
+      // Memastikan variabel environment tersedia di runtime
+      "process.env": env,
+    },
+  };
+});
